4da88dd21757e5556d49da1575470628908f1647
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2025-04-08 22:31:26 +0700

    Fix

--- src/enum/carousel.js
+++ src/enum/carousel.js
@@ -1,5 +1,9 @@
 export const CAROUSEL = [
     {
+        img: "/home01.jpg",
+        name:'Học viện Quân y tổ chức Lễ kỷ niệm 75 năm Ngày truyền thống Học viện Quân y (3/3/1948 - 3/3/2023)',
+    },
+    {
         img: "/carousel_01.jpg",
         name:'Học viện Quân y phát động Tết trồng cây "Đời đời nhớ ơn Bác Hồ" Xuân Ất Tỵ 2023',
     },
